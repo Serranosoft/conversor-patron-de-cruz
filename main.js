86d8e7cde0da9098fe95807ee07a9007ec53a0f6
Manuel Scholz <manuel.serrano943@gmail.com>
2024-11-01 18:23:54 +0100

    Feat: up

--- main.js
+++ main.js
@@ -327,3 +327,9 @@ function getGroupMarkupString(groups) {
     });
     return res;
 }
+
+
+window.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+    console.log('Data received from React Native:', data);
+  });
